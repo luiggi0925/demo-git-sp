a977ce243bfd7c910d65051386e38b68fdf1beb1
Luiggi Mendoza <luiggi0925@gmail.com>
2021-02-23 20:39:37 -0500

    Setup proyecto springboot

---
+++ src/main/java/com/cmaccusco/primerproyecto/PrimerProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.cmaccusco.primerproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmaccusco/primerproyecto/PrimerProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmaccusco.primerproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
